c9a8d867bf440832b0af556faa04b280db7a9997
Garton67 <50034619+Garton67@users.noreply.github.com>
2023-01-01 15:34:35 -0600

    my first commit

---
+++ app.js
@@ -0,0 +1,146 @@
+const boyNames = [
+  "Hendrix",
+  "Miles",
+  "Eli",
+  "Jonah",
+  "Elijah",
+  "Oakley",
+  "Rosia",
+  "Jasper",
+  "Axel",
+  "Ellis",
+  "Soren",
+  "Arlo",
+  "Forest",
+  "Revali",
+  "Calico",
+];
+
+const girlNames = [
+  "Lacee",
+  "Olive",
+  "Xiomara",
+  "Juniper",
+  "Summer",
+  "Luna",
+  "Demi",
+  "Ruby",
+  "Lily",
+  "Lilith",
+  "Riley",
+  "Autumn",
+  "Tatum",
+  "Eris",
+  "London",
+  "Rylee",
+  "Brooklyn",
+  "Mynth",
+  "Elle",
+  "Lux",
+  "Sundae",
+  "Mipha",
+  "Glory",
+  "Kathy",
+  "Saylor",
+  "Solara",
+  "Levy",
+  "Sethra",
+  "Navy",
+  "Nova",
+  "Ember",
+  "Quinn",
+  "Rayne",
+  "Atlas",
+  "Millie",
+];
+
+const secondNames = [
+  "Maples",
+  "Winchester",
+  "Holkiday",
+  "Pearson",
+  "Evans",
+  "Storey",
+  "Garton",
+];
+
+const petNames = [
+  "Kenai",
+  "Marshmello",
+  "Salem",
+  "Eris",
+  "Mango",
+  "Gumbo",
+  "Wisp",
+  "Kenai",
+  "Marshmello",
+  "Salem",
+  "Eris",
+  "Mango",
+  "Gumbo",
+  "Wisp",
+  "Kenai",
+  "Marshmello",
+  "Salem",
+  "Eris",
+  "Mango",
+  "Gumbo",
+  "Wisp",
+  "Kenai",
+  "Marshmello",
+  "Salem",
+  "Eris",
+  "Mango",
+  "Gumbo",
+  "Wisp",
+];
+
+// Random Boy Name
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const getRandomBoyName = () =>
+  `${boyNames[getRandomNumber(boyNames.length)]} ${
+    secondNames[getRandomNumber(secondNames.length)]
+  }`;
+
+const setRandomBoyName = () => {
+  document.getElementById("random-name").innerText = getRandomBoyName();
+};
+
+document
+  .getElementById("generateBoy")
+  .addEventListener("click", setRandomBoyName);
+
+setRandomBoyName();
+
+// Random Girl Name
+
+const getRandomGirlName = () =>
+  `${girlNames[getRandomNumber(girlNames.length)]} ${
+    secondNames[getRandomNumber(secondNames.length)]
+  }`;
+
+const setRandomGirlName = () => {
+  document.getElementById("random-name").innerText = getRandomGirlName();
+};
+
+document
+  .getElementById("generateGirl")
+  .addEventListener("click", setRandomGirlName);
+
+setRandomGirlName();
+
+// Random Pet Name
+
+const getRandomPetName = () => `${petNames[getRandomNumber(petNames.length)]}`;
+
+const setRandomPetName = () => {
+  document.getElementById("random-name").innerText = getRandomPetName();
+};
+
+document
+  .getElementById("generatePet")
+  .addEventListener("click", setRandomPetName);
+
+setRandomGirlName();
---
+++ readme.md
@@ -0,0 +1,7 @@
+Spa Website
+Two column approach
+
+function:
+return a pair of names from an array of firstnames and a array of lastnames from inside a object named names.
+
+combine the first with the last name with a space between
