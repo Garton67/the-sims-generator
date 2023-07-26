793270f50afb30629950be75e4519a654ca45600
Garton67 <50034619+Garton67@users.noreply.github.com>
2023-07-26 18:08:16 -0500

    added name input functionality via local storage

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const boyNames = [
   "Forest",
   "Revali",
   "Calico",
+  // Add more boy names here
 ];
 
 const girlNames = [
@@ -52,16 +53,7 @@ const girlNames = [
   "Rayne",
   "Atlas",
   "Millie",
-];
-
-const secondNames = [
-  "Maples",
-  "Winchester",
-  "Holiday",
-  "Pearson",
-  "Evans",
-  "Storey",
-  "Garton",
+  // Add more girl names here
 ];
 
 const petNames = [
@@ -81,39 +73,141 @@ const petNames = [
   "Navy",
   "Cobweb",
   "Toadflax",
+  // Add more pet names here
 ];
 
-      const getRandomNumber = (max) => Math.floor(Math.random() * max);
+const secondNames = [
+  "Maples",
+  "Winchester",
+  "Holiday",
+  "Pearson",
+  "Evans",
+  "Storey",
+  "Garton",
+  // Add more second names here
+];
 
-      const getRandomName = (namesArray) =>
-        `${namesArray[getRandomNumber(namesArray.length)]} ${
-          secondNames[getRandomNumber(secondNames.length)]
-        }`;
+// Function to get a random number between 0 and max (exclusive)
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
-      const setRandomName = (name) => {
-        document.getElementById("random-name").innerText = name;
-      };
+// Function to get a random name from the names array
+const getRandomName = (namesArray, isPet = false) => {
+  const randomFirstName = namesArray[getRandomNumber(namesArray.length)];
+  if (isPet) {
+    return randomFirstName;
+  } else {
+    const randomLastName = secondNames[getRandomNumber(secondNames.length)];
+    return `${randomFirstName} ${randomLastName}`;
+  }
+};
 
-      document.getElementById("generateBoy").addEventListener("click", () => {
-        const randomBoyName = getRandomName(boyNames);
-        setRandomName(randomBoyName);
-      });
+// Function to set the random name on the page
+const setRandomName = (name) => {
+  document.getElementById("random-name").innerText = name;
+};
 
-      document.getElementById("generateGirl").addEventListener("click", () => {
-        const randomGirlName = getRandomName(girlNames);
-        setRandomName(randomGirlName);
-      });
+// Event listeners for generating names
+document.getElementById("generateBoy").addEventListener("click", () => {
+  const randomBoyName = getRandomName(boyNames);
+  setRandomName(randomBoyName);
+});
 
-      document.getElementById("generatePet").addEventListener("click", () => {
-        const randomPetName = getRandomName(petNames);
-        setRandomName(randomPetName);
-      });
+document.getElementById("generateGirl").addEventListener("click", () => {
+  const randomGirlName = getRandomName(girlNames);
+  setRandomName(randomGirlName);
+});
 
-      const svgContainer = document.getElementById("svg");
-      const animItem = bodymovin.loadAnimation({
-        wrapper: svgContainer,
-        animType: "svg",
-        loop: false,
-        autoplay: false,
-        path: "https://assets2.lottiefiles.com/packages/lf20_u4yrau.json",
-      });
+document.getElementById("generatePet").addEventListener("click", () => {
+  const randomPetName = getRandomName(petNames, true);
+  setRandomName(randomPetName);
+});
+
+// Function to handle form submission and add a name to the list and corresponding array
+const handleFormSubmission = (event) => {
+  event.preventDefault();
+  const newName = document.getElementById("nameInput").value.trim();
+  const nameType = document.getElementById("nameType").value;
+
+  if (newName !== "") {
+    addNameToList(newName, nameType);
+    document.getElementById("nameInput").value = "";
+  }
+};
+
+// Event listener for form submission
+document
+  .getElementById("nameForm")
+  .addEventListener("submit", handleFormSubmission);
+
+// Function to add a name to the list and corresponding array
+function addNameToList(name, type) {
+  const listItem = document.createElement("div");
+  listItem.textContent = name;
+
+  // Add the new name to the corresponding array
+  switch (type) {
+    case "Girl":
+      girlNames.push(name);
+      break;
+    case "Boy":
+      boyNames.push(name);
+      break;
+    case "Pet":
+      petNames.push(name);
+      break;
+    default:
+      break;
+  }
+
+  // Save the updated names array to local storage
+  saveNamesToLocalStorage();
+
+  // Append the new name to the nameList only if it was added by the user
+  nameList.appendChild(listItem);
+}
+
+// Function to save the names arrays to local storage
+function saveNamesToLocalStorage() {
+  localStorage.setItem("Girl", JSON.stringify(girlNames));
+  localStorage.setItem("Boy", JSON.stringify(boyNames));
+  localStorage.setItem("Pet", JSON.stringify(petNames));
+}
+
+// Populate the name list when the app loads
+function populateNameList() {
+  // Clear the nameList before repopulating it
+  nameList.innerHTML = "";
+
+  if (localStorage.getItem("Girl")) {
+    const girlNames = JSON.parse(localStorage.getItem("Girl"));
+    girlNames.forEach((name) => {
+      // Only add names to the nameList that were added by the user
+      if (!girlNames.includes(name)) {
+        addNameToList(name, "Girl");
+      }
+    });
+  }
+
+  if (localStorage.getItem("Boy")) {
+    const boyNames = JSON.parse(localStorage.getItem("Boy"));
+    boyNames.forEach((name) => {
+      // Only add names to the nameList that were added by the user
+      if (!boyNames.includes(name)) {
+        addNameToList(name, "Boy");
+      }
+    });
+  }
+
+  if (localStorage.getItem("Pet")) {
+    const petNames = JSON.parse(localStorage.getItem("Pet"));
+    petNames.forEach((name) => {
+      // Only add names to the nameList that were added by the user
+      if (!petNames.includes(name)) {
+        addNameToList(name, "Pet");
+      }
+    });
+  }
+}
+
+// Populate the name list when the app loads
+populateNameList();
